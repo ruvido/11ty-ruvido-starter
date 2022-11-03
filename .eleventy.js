5ce9c30dffdc0d4b7ca4a821bea0c70f1e0a5619
ruvido <ruvido@gmail.com>
2022-11-03 09:01:25 +0100

    fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css")
+	eleventyConfig.addCollection("posts", function(collectionApi) {
+		return collectionApi.getFilteredByGlob("_posts/*.md");
+		// return collectionApi.getFilteredByGlob(["posts/*.md", "notes/*.md"]);
+	});
 }
